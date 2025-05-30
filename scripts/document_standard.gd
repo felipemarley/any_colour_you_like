db9464bf1ee897c09bce4cf4e57320c2238f9a45
felipemarley <felipemarley34@gmail.com>
2025-05-30 17:21:13 -0300

    feat: draggable document

--- scripts/document_standard.gd
+++ scripts/document_standard.gd
@@ -1,11 +1,18 @@
 class_name Document
 extends Panel
 
-var is_dragged: bool = false 
-var can_drag: bool = false 
-@export_multiline var document_text = ""
+var is_dragging := false
+var drag_offset := Vector2.ZERO
 
-func _process(delta: float) -> void:
-	pass
-func _ready() -> void: 
-	pass
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				is_dragging = true
+				drag_offset = get_global_mouse_position() - global_position
+				move_to_front()
+			else:
+				is_dragging = false
+
+	elif event is InputEventMouseMotion and is_dragging:
+		global_position = get_global_mouse_position() - drag_offset
