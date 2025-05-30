507b18691dedc42a54d821ab0d8e8e070f74b545
felipemarley <felipemarley34@gmail.com>
2025-05-30 18:25:52 -0300

    feat: defined documents

--- scripts/document_scene.gd
+++ scripts/document_scene.gd
@@ -1,17 +1,1 @@
 extends Panel
-
-var dragging_document: Document = null
-
-func _ready() -> void:
-	pass
-
-#auxiliar functions
-func _get_drag_data(at_position: Vector2) -> Variant:
-	var document = Document.new()
-	document.size = Vector2(100, 50)
-	set_drag_preview(document)
-	return document
-func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	return data is Document
-func _drop_data(at_position: Vector2, data: Variant) -> void:
-	dragging_document = data
--- scripts/document_standard.gd
+++ scripts/document_standard.gd
@@ -1,8 +1,16 @@
 class_name Document
 extends Panel
 
+@export_multiline var document_text: = ""
+@export_node_path("RichTextLabel") var text_path: NodePath = ""
+@export_node_path("TextureRect") var texture_path: NodePath = ""
+
 var is_dragging := false
 var drag_offset := Vector2.ZERO
+
+func _ready() -> void:
+	get_node(text_path).text = document_text
+	get_node(text_path).fit_content = true
 
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -10,9 +18,8 @@ func _gui_input(event: InputEvent) -> void:
 			if event.pressed:
 				is_dragging = true
 				drag_offset = get_global_mouse_position() - global_position
-				move_to_front()
+				move_to_front() 
 			else:
 				is_dragging = false
-
 	elif event is InputEventMouseMotion and is_dragging:
 		global_position = get_global_mouse_position() - drag_offset
