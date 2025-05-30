1a99c105ad13a08fd6769bc8ebdb5ba7244945d7
felipemarley <felipemarley34@gmail.com>
2025-05-30 14:25:21 -0300

    inicio drag_document

---
+++ scripts/document_standard.gd
@@ -0,0 +1,11 @@
+class_name Document
+extends Panel
+
+var is_dragged: bool = false 
+var can_drag: bool = false 
+@export_multiline var document_text = ""
+
+func _process(delta: float) -> void:
+	pass
+func _ready() -> void: 
+	pass
