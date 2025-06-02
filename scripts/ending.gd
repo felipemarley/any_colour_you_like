c48ffae017eb9acb40f39ea4901781941a0c7eb5
felipemarley <felipemarley34@gmail.com>
2025-06-02 00:10:52 -0300

    menu_background art

--- scripts/ending.gd
+++ scripts/ending.gd
@@ -1,14 +1,15 @@
 extends ColorRect
 
 func _process(delta: float) -> void:
-	$texto_resumo_final.text = "FIM DO PRIMEIRO DIA
+	$texto_resumo_final.text = (
+	"FIM DO PRIMEIRO DIA\n\n" +
+	"VOCÊ APROVOU [color=green]" + str(Global.aprovados) + "[/color] PESSOAS\n" +
+	"VOCÊ REPROVOU [color=red]" + str(Global.reprovados) + "[/color] PESSOAS\n\n" +
+	"VOCÊ ESTÁ MESMO LEVANDO O SISTEMA A SÉRIO?\n\n" +
+	"OBRIGADO POR JOGAR NOSSA ALPHA"
+)
 
-VOCÊ APROVOU "+str(Global.aprovados)+" PESSOAS
-VOCÊ REPROVOU "+str(Global.reprovados)+" PESSOAS
 
-VOCÊ ESTÁ MESMO LEVANDO O SISTEMA A SÉRIO?
-
-OBRIGADO POR JOGAR NOSSA ALPHA"
 
 func _on_voltar_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/screens/main_menu.tscn")
