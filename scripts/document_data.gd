c4585843cbf8020d13b5794d21c6870f3808f681
Marina Torres <marilimatorres26@gmail.com>
2025-06-02 15:14:05 -0300

    feat: green-1: documentacao

--- scripts/document_data.gd
+++ scripts/document_data.gd
@@ -4,7 +4,7 @@ static func get_documentos_data():
 	return {
 		"RG": {
 			"textos": {
-					"blue-01": "ZYX'ARR KLYTHON
+					"blue-01": "MARTIANA PERUA
 								RG - 746.900-MR
 								ETNIA - CIANO
 								DATA NAS.: 17/05/2002
@@ -25,7 +25,13 @@ DATA NAS.: 30/11/1998
 DATA EMISSÃO: 22/01/2018
 VALIDADE: 22/01/2028
 ",
-				"green-01": "RG 04 - Verde",
+				"green-01": "FILTROPINO PINO
+RG - 999.100-GR
+ETNIA - VERDE 
+DATA NAS.: 05/05/2005
+VALIDADE: 22/01/2025
+",
+
 				"purple-01": "RG 05 - Roxo",
 				"red-01": "RG 06 - Vermelho",
 				"yellow-01": "RG 07 - Amarelo"
@@ -35,11 +41,11 @@ VALIDADE: 22/01/2028
 		"certidao": {
 			"textos": {
 				"blue-01": "CERTIFICADO DE NASCIMENTO MARCIANO
-NOME: Zyx'arr Klython
+NOME: MARTIANA PERUA
 DATA: 17/05/2002
 LOCAL: Clínica Genética de New Xenthia - Região verde-56
 FILIAÇÃO:
-- PORTADOR GENÉTICO 1: Kly'thon Klython (ETNIA AZUL)
+- PORTADOR GENÉTICO 1: Kly'thon Perua (ETNIA AZUL)
 - PORTADOR GENÉTICO 2: Qw'ena Vlor (ETNIA VERDE)
 MODIFICAÇÕES GENÉTICAS REGISTRADAS: Nenhuma
 ETNIA DECLARADA: CIANO
@@ -50,8 +56,8 @@ NOME: RICHARDESO POLES
 DATA: 12/09/2000
 LOCAL: Zona de Pesca - Colônia Azul-3 
 FILIAÇÃO:
-- PORTADOR GENÉTICO 1: Vex'arn Thorvex (ETNIA AZUL ESCURO)
-- PORTADOR GENÉTICO 2: My'ara Klyth (ETNIA AZUL CLARO)
+- PORTADOR GENÉTICO 1: Vardemir Poles (ETNIA AZUL ESCURO)
+- PORTADOR GENÉTICO 2: Myantia Klyth (ETNIA AZUL CLARO)
 MODIFICAÇÕES GENÉTICAS REGISTRADAS: CHIFRUDO
 ETNIA DECLARADA: AZUL
 CÓDIGO BIOLÓMICO: MR-1124-TB",
@@ -63,11 +69,23 @@ LOCAL: Zona central - Colônia Azul-1
 
 FILIAÇÃO:
 - PORTADOR GENÉTICO 1: Zy'than Zythar (ETNIA AZUL)
-- PORTADOR GENÉTICO 2: Or'vax Zythar (VERMELHO CLARO)
+- PORTADOR GENÉTICO 2: Orlsa Zythar (VERMELHO CLARO)
+MODIFICAÇÕES GENÉTICAS REGISTRADAS: Nenhuma
 ETNIA DECLARADA: AZUL CLARO
 CÓDIGO BIOLÔMICO: MR-0001-DX",
 
-				"green-01": "Certidão 04 - Verde",
+				"green-01": "CERTIFICADO DE NASCIMENTO MARCIANO
+NOME: FILTROPINO PINO
+DATA: 05/05/2005
+LOCAL: Sul da colonia - Colônia Verde-1 
+
+FILIAÇÃO:
+- PORTADOR GENÉTICO 1: Xerox Qirn (VERDE)
+- PORTADOR GENÉTICO 2: In'dex Pino (VERDE CLARO)
+MODIFICAÇÕES GENÉTICAS REGISTRADAS: Nenhuma
+ETNIA DECLARADA: VERDE
+CÓDIGO BIOLÔMICO: MR-0001-DX",
+
 				"purple-01": "Certidão 05 - Roxo",
 				"red-01": "Certidão 06 - Vermelho",
 				"yellow-01": "Certidão 07 - Amarelo"
@@ -76,8 +94,8 @@ CÓDIGO BIOLÔMICO: MR-0001-DX",
 				"blue-01": "res://assets/documents/certidao-nasc-002.png",
 				"blue-02": "res://assets/documents/certidao-nasc-004.png",
 				"blue-03": "res://assets/documents/certidao-nasc-004.png",
+				"green-01": "res://assets/documents/certidao-nasc-002.png",
 				
-				"green-01": "res://assets/documents/certidao-nasc-004.png",
 				"purple-01": "res://assets/documents/certidao-nasc-001.png",
 				"red-01": "res://assets/documents/certidao-nasc-002.png",
 				"yellow-01": "res://assets/documents/certidao-nasc-003.png"
@@ -88,7 +106,7 @@ CÓDIGO BIOLÔMICO: MR-0001-DX",
 				"blue-01": "FORMULÁRIO DE AUTOCLASSIFICAÇÃO ÉTNICA
 SOB PENA DE PERDA DE DIREITOS CIVIS POR FALSA DECLARAÇÃO
 
-Eu, Zyx'arr Klython, declaro sob juramento genético que:
+Eu, MARTIANA PERUA, declaro sob juramento genético que:
 
 [X] OPTO pelo sistema de cotas para AZUIS (Art.7º)
 [ ] OPTO pelo sistema de cotas para VERMELHOS (Art.12º)
@@ -114,7 +132,7 @@ Justificativa:
 'Meu código biolômico comprova ancestralidade azul total.'
 
 ASSINATURA BIOMÉTRICA: (padrão de DNA verificado)
-DATA MARCIANA: 16/01/2025",
+DATA MARCIANA: 15/01/2025",
 
 
 				"blue-03": "FORMULÁRIO DE AUTOCLASSIFICAÇÃO ÉTNICA
@@ -132,7 +150,22 @@ Por esse motivo desejo as duas cotas.'
 ASSINATURA BIOMÉTRICA: (padrão de DNA verificado)
 DATA MARCIANA: 15/01/2025",
 
-				"green-01": "Declaração 04 - Verde",
+				"green-01": "FORMULÁRIO DE AUTOCLASSIFICAÇÃO ÉTNICA
+SOB PENA DE PERDA DE DIREITOS CIVIS POR FALSA DECLARAÇÃO
+
+Eu, FILTROPINO PINO, declaro sob juramento genético que:
+
+[X] OPTO pelo sistema de cotas para AZUIS (Art.7º)
+[ ] OPTO pelo sistema de cotas para VERMELHOS (Art.12º)
+
+Justificativa: 
+'Meu código biolômico comprova ancestralidade azul no que se diz 3° grau,
+meu bisavó por parte de mão era azul claro, e isso fica evidente pelo meu
+nariz.'
+
+ASSINATURA BIOMÉTRICA: (padrão de DNA verificado)
+DATA MARCIANA: 15/01/2025",
+
 				"purple-01": "Declaração 05 - Roxo",
 				"red-01": "Declaração 06 - Vermelho",
 				"yellow-01": "Declaração 07 - Amarelo"
