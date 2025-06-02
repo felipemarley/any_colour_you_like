f0ceaeb6e585c9472c4d855b6cc25eed01946afe
Marina Torres <marilimatorres26@gmail.com>
2025-06-02 14:48:19 -0300

    feat: 2 documentações ja feitas

--- scripts/document_data.gd
+++ scripts/document_data.gd
@@ -4,8 +4,20 @@ static func get_documentos_data():
 	return {
 		"RG": {
 			"textos": {
-				"blue-01": "RG 01 - Azul Claro",
-				"blue-02": "RG 02 - Azul Médio",
+					"blue-01": "ZYX'ARR KLYTHON
+								RG - 746.900-MR
+								ETNIA - CIANO
+								DATA NAS.: 17/05/2002
+								DATA DE EMISSÃO: 30/08/2024
+								VALIDADE: 30/08/2034",
+								
+				"blue-02": "RICHARDESO POLES
+RG - 555.200-MR
+ETNIA - AZUL 
+DATA NAS.: 12/09/2000
+DATA EMISSÃO: 03/03/2020
+VALIDADE: 03/03/2030",
+
 				"blue-03": "RG 03 - Azul Escuro",
 				"green-01": "RG 04 - Verde",
 				"purple-01": "RG 05 - Roxo",
@@ -16,8 +28,28 @@ static func get_documentos_data():
 		},
 		"certidao": {
 			"textos": {
-				"blue-01": "Certidão 01 - Azul Claro",
-				"blue-02": "Certidão 02 - Azul Médio",
+				"blue-01": "CERTIFICADO DE NASCIMENTO MARCIANO
+NOME: Zyx'arr Klython
+DATA: 17/05/2002
+LOCAL: Clínica Genética de New Xenthia - Região verde-56
+FILIAÇÃO:
+- PORTADOR GENÉTICO 1: Kly'thon Klython (ETNIA AZUL)
+- PORTADOR GENÉTICO 2: Qw'ena Vlor (ETNIA VERDE)
+MODIFICAÇÕES GENÉTICAS REGISTRADAS: Nenhuma
+ETNIA DECLARADA: CIANO
+CÓDIGO BIOLÓMICO: MR-0092-XT",
+				
+				"blue-02": "CERTIFICADO DE NASCIMENTO MARCIANO
+NOME: RICHARDESO POLES
+DATA: 12/09/2000
+LOCAL: Zona de Pesca - Colônia Azul-3 
+FILIAÇÃO:
+- PORTADOR GENÉTICO 1: Vex'arn Thorvex (ETNIA AZUL ESCURO)
+- PORTADOR GENÉTICO 2: My'ara Klyth (ETNIA AZUL CLARO)
+MODIFICAÇÕES GENÉTICAS REGISTRADAS: CHIFRUDO
+ETNIA DECLARADA: AZUL
+CÓDIGO BIOLÓMICO: MR-1124-TB",
+
 				"blue-03": "Certidão 03 - Azul Escuro",
 				"green-01": "Certidão 04 - Verde",
 				"purple-01": "Certidão 05 - Roxo",
@@ -25,8 +57,8 @@ static func get_documentos_data():
 				"yellow-01": "Certidão 07 - Amarelo"
 			},
 			"texturas": {
-				"blue-01": "res://assets/documents/certidao-nasc-001.png",
-				"blue-02": "res://assets/documents/certidao-nasc-002.png",
+				"blue-01": "res://assets/documents/certidao-nasc-002.png",
+				"blue-02": "res://assets/documents/certidao-nasc-004.png",
 				"blue-03": "res://assets/documents/certidao-nasc-003.png",
 				"green-01": "res://assets/documents/certidao-nasc-004.png",
 				"purple-01": "res://assets/documents/certidao-nasc-001.png",
@@ -36,8 +68,36 @@ static func get_documentos_data():
 		},
 		"declaracao": {
 			"textos": {
-				"blue-01": "Declaração 01 - Azul Claro",
-				"blue-02": "Declaração 02 - Azul Médio",
+				"blue-01": "FORMULÁRIO DE AUTOCLASSIFICAÇÃO ÉTNICA
+SOB PENA DE PERDA DE DIREITOS CIVIS POR FALSA DECLARAÇÃO
+
+Eu, Zyx'arr Klython, declaro sob juramento genético que:
+
+[X] OPTO pelo sistema de cotas para AZUIS (Art.7º)
+[ ] OPTO pelo sistema de cotas para VERMELHOS (Art.12º)
+
+Justificativa: 
+'Meu código biolômico comprova ancestralidade azul,
+apesar da contribuição genética verde da portadora Qw'ena Vlor.
+Meu fenótipo é azul, como atesta meu selo epidermal.'
+
+ASSINATURA BIOMÉTRICA: (padrão de DNA verificado)
+DATA MARCIANA: 15/01/2025",
+
+				"blue-02": "FORMULÁRIO DE AUTOCLASSIFICAÇÃO ÉTNICA
+SOB PENA DE PERDA DE DIREITOS CIVIS POR FALSA DECLARAÇÃO
+
+Eu, RICHARDESO POLES, declaro sob juramento genético que:
+
+[X] OPTO pelo sistema de cotas para AZUIS (Art.7º)
+[ ] OPTO pelo sistema de cotas para VERMELHOS (Art.12º)
+
+Justificativa: 
+'Meu código biolômico comprova ancestralidade azul total.'
+
+ASSINATURA BIOMÉTRICA: (padrão de DNA verificado)
+DATA MARCIANA: 16/01/2025",
+
 				"blue-03": "Declaração 03 - Azul Escuro",
 				"green-01": "Declaração 04 - Verde",
 				"purple-01": "Declaração 05 - Roxo",
